7fd94b676da14f888539181f267be17e1bb6fed5
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-10-19 11:55:21 +0300

    MapWorld fixx

--- src/main/java/SimulationWorld/Map/MapWorld.java
+++ src/main/java/SimulationWorld/Map/MapWorld.java
@@ -7,11 +7,12 @@ import SimulationWorld.Entitus.Creatures.Predator;
 import SimulationWorld.Icon.Icons;
 import lombok.Getter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MapWorld { // Класс реализует карту мира
     @Getter private static Class [] classes = new Class[]{Grass.class, Herbivore.class, Predator.class, Rock.class, Tree.class};  // Массив классов мира
-    @Getter  private HashMap<Coordinates, Entity> mapWorld = new HashMap<>();                                           // Карта мира с объектами и Getter
-    public int HerbDead = 0, PredDead = 0, GrassEat = 0;                                                           // счетчики кол-ва смертей/съеденных фруктов
+    @Getter  private HashMap<Coordinates, Entity> mapWorld = new HashMap<>();                                                    // Карта мира с объектами и Getter
+    public int HerbDead = 0, PredDead = 0, GrassEat = 0;                                                                        // счетчики кол-ва смертей/съеденных фруктов
     public boolean isSquareEmpty(Coordinates coordinates) {
         return !mapWorld.containsKey(coordinates);
     }                                                    // Проверяет пустая ли ячейка
@@ -25,22 +26,19 @@ public class MapWorld { // Класс реализует карту мира
     public Coordinates getRandomCoordinates() {
         return getEmptySquare().isEmpty()?null:getEmptySquare().get((int) (Math.random()*getEmptySquare().size()));
     }                                                        // Получить случайные координаты
-    public <T> HashMap<Coordinates, T> getEntityesOfType(Class<T> type) {
-        var map = new HashMap<Coordinates, T>();
-        for (Map.Entry<Coordinates, Entity> pair : mapWorld.entrySet())
-            if (type.isInstance(pair.getValue()) && pair.getValue().icon != Icons.DeadObject[0] && pair.getValue().icon != Icons.DeadObject[1])
-                map.put(pair.getKey(), (T) pair.getValue());
-        return map;
-    }                            // Получить Координаты и существ определенного типа
+    public <T> Set<Coordinates> getEntityesOfType(Class<T> type) {
+       return  mapWorld.keySet().stream().filter(c-> getEntity(c).getClass() == type).collect(Collectors.toSet());
+    }                            // Получить Координаты  существ определенного типа
     public void removeCreature(Creature creature, Coordinates coordinates) { // Переставить животное
-        this.mapWorld.remove(creature.coordinates);
-        this.setEntity(coordinates, creature);
+        mapWorld.remove(creature.coordinates);
+        setEntity(coordinates, creature);
     }                       // Передвинуть существо на карте
     public ArrayList<Coordinates> getEmptySquare() { // Возвращает координаты пустых ячеек на карте
         var emptySquare = new ArrayList<Coordinates>();
         for (int rank = Coordinates.getMaxSize(); rank >= 1; rank--) {
             for (int file = Coordinates.getMaxSize(); file >=1; file--) {
-                if (isSquareEmpty(new Coordinates(file, rank))) emptySquare.add(new Coordinates(file, rank));
+                Coordinates coordinates = new Coordinates(file,rank);
+                if (isSquareEmpty(coordinates)) emptySquare.add(coordinates);
             }
         }
         return emptySquare;
