d3c0a67dc9df14ec5fb8a73bc1ada6f5c0af6077
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-10-23 11:48:40 +0300

    refactoring

--- src/main/java/SimulationWorld/Entitus/Entity.java
+++ src/main/java/SimulationWorld/Entitus/Entity.java
@@ -4,12 +4,15 @@ import SimulationWorld.Map.Coordinates;
 public abstract class Entity {       // суперкласс для  всех объектов мира
     public Coordinates coordinates;
     public String icon;
+    public String path;
 
     public Entity(Coordinates coordinates) {
         this.coordinates = coordinates;
         this.icon = getRandomIcon();
     }
     protected abstract String getRandomIcon(); // Получить случайную иконку
+
+
 
 
 }
